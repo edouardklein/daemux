0e586a8fce2225fbe201b173cf5a8a7d47698a3e
Edouard <edouard.klein@gendarmerie.interieur.gouv.fr>
2017-03-08 17:03:09 +0100

    Add libtmux as a dependancy

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-install_requires = ['sphinx', 'doctest', 'flake8']
+install_requires = ['sphinx', 'doctest', 'flake8', 'libtmux']
 dependency_links = []
 
 setup(
